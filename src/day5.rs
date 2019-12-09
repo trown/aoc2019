30e1b314f97f1835da0d50363bf03480b685faaa
John Trowbridge <john@onna.com>
2019-12-09 02:31:07 +0000

    day 5 part 2

--- src/day5.rs
+++ src/day5.rs
@@ -29,107 +29,118 @@ impl Program {
         }
     }
 
-    pub fn run_diagnostics(&mut self) -> i32 {
-        while self.next() {}
+    pub fn run_diagnostics(&mut self, debug: bool) -> i32 {
+        if debug {
+            println!(
+                "first 30: {:?}",
+                &self.data[self.pointer..self.pointer + 30],
+            );
+        }
+        while self.next() {
+            if debug {
+                println!(
+                    "========================================================================="
+                );
+                println!(
+                    "pointer: {} next_data: {:?}",
+                    self.pointer,
+                    &self.data[self.pointer..]
+                );
+            }
+        }
         self.output
+    }
+    pub fn read(&mut self, mode: &u8, ptr: usize) -> i32 {
+        if mode == &0u8 {
+            self.data[self.data[ptr] as usize]
+        } else {
+            self.data[ptr]
+        }
     }
 
     // Continues the execution of the program, returning
     // true if the program should continue, false if it should stop
     pub fn next(&mut self) -> bool {
-        let opcode = self.data[self.pointer];
+        let instruction = format!("{:0>6}", &self.data[self.pointer]);
+        let opcode = &instruction[4..6];
+        let mode_a = &instruction[3..4].parse::<u8>().unwrap();
+        //println!("{}", mode_a);
+        let mode_b = &instruction[2..3].parse::<u8>().unwrap();
+        //println!("{}", mode_b);
         let res = match opcode {
-            00001 => {
-                let idx_a = self.data[self.pointer + 1] as usize;
-                let idx_b = self.data[self.pointer + 2] as usize;
-                let idx_c = self.data[self.pointer + 3] as usize;
-                let val = self.data[idx_a] + self.data[idx_b];
-                self.data[idx_c] = val;
+            "01" => {
+                let read_a = self.read(mode_a, self.pointer + 1);
+                let read_b = self.read(mode_b, self.pointer + 2);
+                let w_ptr = self.data[self.pointer + 3] as usize;
+                self.data[w_ptr] = read_a + read_b;
                 self.pointer += 4;
                 true
             }
-            00101 => {
-                let idx_a = self.data[self.pointer + 1];
-                let idx_b = self.data[self.pointer + 2] as usize;
-                let idx_c = self.data[self.pointer + 3] as usize;
-                let val = idx_a + self.data[idx_b];
-                self.data[idx_c] = val;
+            "02" => {
+                let read_a = self.read(mode_a, self.pointer + 1);
+                let read_b = self.read(mode_b, self.pointer + 2);
+                let w_ptr = self.data[self.pointer + 3] as usize;
+                self.data[w_ptr] = read_a * read_b;
                 self.pointer += 4;
                 true
             }
-            01001 => {
-                let idx_a = self.data[self.pointer + 1] as usize;
-                let idx_b = self.data[self.pointer + 2];
-                let idx_c = self.data[self.pointer + 3] as usize;
-                let val = self.data[idx_a] + idx_b;
-                self.data[idx_c] = val;
-                self.pointer += 4;
-                true
-            }
-            01101 => {
-                let idx_a = self.data[self.pointer + 1];
-                let idx_b = self.data[self.pointer + 2];
-                let idx_c = self.data[self.pointer + 3] as usize;
-                let val = idx_a + idx_b;
-                self.data[idx_c] = val;
-                self.pointer += 4;
-                true
-            }
-            00002 => {
-                let idx_a = self.data[self.pointer + 1] as usize;
-                let idx_b = self.data[self.pointer + 2] as usize;
-                let idx_c = self.data[self.pointer + 3] as usize;
-                let val = self.data[idx_a] * self.data[idx_b];
-                self.data[idx_c] = val;
-                self.pointer += 4;
-                true
-            }
-            00102 => {
-                let idx_a = self.data[self.pointer + 1];
-                let idx_b = self.data[self.pointer + 2] as usize;
-                let idx_c = self.data[self.pointer + 3] as usize;
-                let val = idx_a * self.data[idx_b];
-                self.data[idx_c] = val;
-                self.pointer += 4;
-                true
-            }
-            01002 => {
-                let idx_a = self.data[self.pointer + 1] as usize;
-                let idx_b = self.data[self.pointer + 2];
-                let idx_c = self.data[self.pointer + 3] as usize;
-                let val = self.data[idx_a] * idx_b;
-                self.data[idx_c] = val;
-                self.pointer += 4;
-                true
-            }
-            01102 => {
-                let idx_a = self.data[self.pointer + 1];
-                let idx_b = self.data[self.pointer + 2];
-                let idx_c = self.data[self.pointer + 3] as usize;
-                let val = idx_a * idx_b;
-                self.data[idx_c] = val;
-                self.pointer += 4;
-                true
-            }
-            3 => {
+            "03" => {
                 let idx_a = self.data[self.pointer + 1] as usize;
                 self.data[idx_a] = self.input;
                 self.pointer += 2;
                 true
             }
-            4 => {
-                let idx_a = self.data[self.pointer + 1] as usize;
-                self.output = self.data[idx_a];
+            "04" => {
+                let read_a = self.read(mode_a, self.pointer + 1);
+                self.output = read_a;
                 self.pointer += 2;
                 true
             }
-            104 => {
-                let idx_a = self.data[self.pointer + 1];
-                self.output = idx_a;
-                self.pointer += 2;
+            "05" => {
+                let read_a = self.read(mode_a, self.pointer + 1);
+                let read_b = self.read(mode_b, self.pointer + 2);
+                if read_a != 0 {
+                    self.pointer = read_b as usize
+                } else {
+                    self.pointer += 3
+                }
                 true
             }
-            99 => false,
+            "06" => {
+                let read_a = self.read(mode_a, self.pointer + 1);
+                let read_b = self.read(mode_b, self.pointer + 2);
+                if read_a == 0 {
+                    self.pointer = read_b as usize
+                } else {
+                    self.pointer += 3
+                }
+                true
+            }
+            "07" => {
+                let read_a = self.read(mode_a, self.pointer + 1);
+                let read_b = self.read(mode_b, self.pointer + 2);
+                let w_ptr = self.data[self.pointer + 3] as usize;
+                if read_a < read_b {
+                    self.data[w_ptr] = 1;
+                } else {
+                    self.data[w_ptr] = 0;
+                }
+                self.pointer += 4;
+                true
+            }
+            "08" => {
+                let read_a = self.read(mode_a, self.pointer + 1);
+                let read_b = self.read(mode_b, self.pointer + 2);
+                let w_ptr = self.data[self.pointer + 3] as usize;
+                if read_a == read_b {
+                    self.data[w_ptr] = 1;
+                } else {
+                    self.data[w_ptr] = 0;
+                }
+                self.pointer += 4;
+                true
+            }
+            "99" => false,
             _ => panic!("you dun goofed"),
         };
         res
@@ -141,7 +152,15 @@ impl Program {
 /// returning the diagnostic code
 fn part_one(input: &[i32]) -> i32 {
     let mut program = Program::new(input.to_vec(), 1);
-    program.run_diagnostics()
+    program.run_diagnostics(false)
+}
+
+#[aoc(day5, part2)]
+/// Solves part one by running the intcode program and
+/// returning the diagnostic code
+fn part_two(input: &[i32]) -> i32 {
+    let mut program = Program::new(input.to_vec(), 5);
+    program.run_diagnostics(true)
 }
 
 #[cfg(test)]
@@ -169,12 +188,51 @@ pub mod tests {
     #[test]
     fn simple_diagnostics() {
         let mut program = Program::new(vec![3, 0, 4, 0, 99], 23);
-        assert_eq!(program.run_diagnostics(), 23);
+        assert_eq!(program.run_diagnostics(false), 23);
     }
 
     #[test]
     fn negatives_allowed() {
         let mut program = Program::new(vec![1101, 100, -1, 4, 0], 23);
-        assert_eq!(program.run_diagnostics(), 0);
+        assert_eq!(program.run_diagnostics(false), 0);
+        program = Program::new(
+            vec![
+                3, 28, 1, 28, 6, 6, 1100, 1, 238, 28, 104, 0, 1102, 72, 20, 27, 1001, 27, -1440,
+                27, 4, 27, 102, 99, 7, 26, 1001, 224, 5, 224, 99,
+            ],
+            1,
+        );
+        assert_eq!(program.run_diagnostics(true), 0);
+    }
+
+    #[test]
+    fn day_5_part_2() {
+        let mut program = Program::new(
+            vec![
+                3, 21, 1008, 21, 8, 20, 1005, 20, 22, 107, 8, 21, 20, 1006, 20, 31, 1106, 0, 36,
+                98, 0, 0, 1002, 21, 125, 20, 4, 20, 1105, 1, 46, 104, 999, 1105, 1, 46, 1101, 1000,
+                1, 20, 4, 20, 1105, 1, 46, 98, 99,
+            ],
+            5,
+        );
+        assert_eq!(program.run_diagnostics(true), 999);
+        program = Program::new(
+            vec![
+                3, 21, 1008, 21, 8, 20, 1005, 20, 22, 107, 8, 21, 20, 1006, 20, 31, 1106, 0, 36,
+                98, 0, 0, 1002, 21, 125, 20, 4, 20, 1105, 1, 46, 104, 999, 1105, 1, 46, 1101, 1000,
+                1, 20, 4, 20, 1105, 1, 46, 98, 99,
+            ],
+            8,
+        );
+        assert_eq!(program.run_diagnostics(true), 1000);
+        program = Program::new(
+            vec![
+                3, 21, 1008, 21, 8, 20, 1005, 20, 22, 107, 8, 21, 20, 1006, 20, 31, 1106, 0, 36,
+                98, 0, 0, 1002, 21, 125, 20, 4, 20, 1105, 1, 46, 104, 999, 1105, 1, 46, 1101, 1000,
+                1, 20, 4, 20, 1105, 1, 46, 98, 99,
+            ],
+            23,
+        );
+        assert_eq!(program.run_diagnostics(true), 1001);
     }
 }
